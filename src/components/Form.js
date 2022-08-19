89c9f3a4a7f0b7ca155f63757d89f580224ce12f
vandu-s <vandana.singandipe@gmail.com>
2022-08-19 16:22:35 +0530

    done with ui of personal details

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,16 +10,52 @@ const Form = () => {
           <h3>Personal Details</h3>
           <div className="form_group_parent">
             <div className="form_group">
-              <label htmlFor="name">Name</label>
-              <input type="text" placeholder="Enter your name" />
+              <label htmlFor="name">
+                Name
+                <span className="star">*</span>
+              </label>
+              <input type="text" placeholder="Enter Name" />
             </div>
             <div className="form_group">
-              <label htmlFor="name">Name</label>
-              <input type="text" placeholder="Enter your name" />
+              <label htmlFor="name">
+                Date of Birth or Age
+                <span className="star">*</span>
+              </label>
+              <input type="text" placeholder="DD/MM/YYYY or Age in Years" />
             </div>
             <div className="form_group">
-              <label htmlFor="name">Name</label>
-              <input type="text" placeholder="Enter your name" />
+              <label htmlFor="sex">
+                Sex
+                <span className="star">*</span>
+              </label>
+              <select name="sex" id="" form="">
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+          </div>
+          <br />
+          <div className="form_group_parent">
+            <div className="form_group">
+              <label htmlFor="name">
+                Mobile
+                <span className="star">*</span>
+              </label>
+              <input type="text" placeholder="Enter Mobile" />
+            </div>
+            <div className="form_group">
+              <label htmlFor="name">
+                Govt Issue ID <span className="star">*</span>
+              </label>
+              <select name=" Govt Issue ID">
+                <option value="Pen Card">Pen Card</option>
+                <option value="Aadhaar Card">Aadhaar Card</option>
+                <option value="Driving License">Driving License</option>
+                <option value="Voter ID">Voter ID</option>
+              </select>
+
+              <input type="text" placeholder="Enter Govt ID" />
             </div>
           </div>
         </form>
